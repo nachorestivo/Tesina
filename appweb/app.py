096187ad10277fcd9a36372c105119d79fff1a3e
Elios356 <ijretivo@escuelasproa.edu.ar>
2025-09-29 09:51:26 -0300

    29/09
    29/09

--- appweb/app.py
+++ appweb/app.py
@@ -39,7 +39,7 @@ def login():
             session['username'] = usuarios['username']
             session['is_admin'] = usuarios['is_admin']
             flash('¡Inicio de sesión exitoso!', 'success')
-            return redirect(url_for('catalogo'))
+            return redirect(url_for('index'))
         else:
             flash('Usuario o contraseña incorrectos', 'error')
     return render_template('login.html')
